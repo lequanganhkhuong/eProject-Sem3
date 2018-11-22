4fcaebe9c92f2e794c46e3109b1ed3ead4ef8a89
Khương Lê <anhkhuong1412@gmail.com>
2018-11-22 20:16:56 +0700

    22/11 update

---
+++ eProject-Sem3/ZuLuCommerce/ZuLuCommerce/Areas/ADMIN/Controllers/CategoriesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ZuLuCommerce.Models;
+
+namespace ZuLuCommerce.Areas.ADMIN.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private eCommerceEntities db = new eCommerceEntities();
+
+        // GET: ADMIN/Categories
+        public ActionResult Index()
+        {
+            var categories = db.Categories.Include(c => c.Topic);
+            return View(categories.ToList());
+        }
+
+        // GET: ADMIN/Categories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // GET: ADMIN/Categories/Create
+        public ActionResult Create()
+        {
+            ViewBag.TopicId = new SelectList(db.Topics, "Id", "TopicName");
+            return View();
+        }
+
+        // POST: ADMIN/Categories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description,TopicId")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.TopicId = new SelectList(db.Topics, "Id", "TopicName", category.TopicId);
+            return View(category);
+        }
+
+        // GET: ADMIN/Categories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TopicId = new SelectList(db.Topics, "Id", "TopicName", category.TopicId);
+            return View(category);
+        }
+
+        // POST: ADMIN/Categories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,TopicId")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.TopicId = new SelectList(db.Topics, "Id", "TopicName", category.TopicId);
+            return View(category);
+        }
+
+        // GET: ADMIN/Categories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: ADMIN/Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Category category = db.Categories.Find(id);
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ eProject-Sem3/ZuLuCommerce/ZuLuCommerce/Areas/ADMIN/Controllers/OrdersController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ZuLuCommerce.Models;
+
+namespace ZuLuCommerce.Areas.ADMIN.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private eCommerceEntities db = new eCommerceEntities();
+
+        // GET: ADMIN/Orders
+        public ActionResult Index()
+        {
+            var orders = db.Orders.Include(o => o.Customer).Include(o => o.Employee).Include(o => o.OrderStatus).Include(o => o.Shipment);
+            return View(orders.ToList());
+        }
+
+        // GET: ADMIN/Orders/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // GET: ADMIN/Orders/Create
+        public ActionResult Create()
+        {
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "FirstName");
+            ViewBag.EmployeeId = new SelectList(db.Employees, "Id", "FirstName");
+            ViewBag.StatusId = new SelectList(db.OrderStatuses, "Id", "Name");
+            ViewBag.ShipmentId = new SelectList(db.Shipments, "Id", "ShippingAddress");
+            return View();
+        }
+
+        // POST: ADMIN/Orders/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,CreatedDate,StatusId,Description,PaymentType,CustomerId,EmployeeId,ShipmentId,Discount,Tax")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Orders.Add(order);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "FirstName", order.CustomerId);
+            ViewBag.EmployeeId = new SelectList(db.Employees, "Id", "FirstName", order.EmployeeId);
+            ViewBag.StatusId = new SelectList(db.OrderStatuses, "Id", "Name", order.StatusId);
+            ViewBag.ShipmentId = new SelectList(db.Shipments, "Id", "ShippingAddress", order.ShipmentId);
+            return View(order);
+        }
+
+        // GET: ADMIN/Orders/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "FirstName", order.CustomerId);
+            ViewBag.EmployeeId = new SelectList(db.Employees, "Id", "FirstName", order.EmployeeId);
+            ViewBag.StatusId = new SelectList(db.OrderStatuses, "Id", "Name", order.StatusId);
+            ViewBag.ShipmentId = new SelectList(db.Shipments, "Id", "ShippingAddress", order.ShipmentId);
+            return View(order);
+        }
+
+        // POST: ADMIN/Orders/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,CreatedDate,StatusId,Description,PaymentType,CustomerId,EmployeeId,ShipmentId,Discount,Tax")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "FirstName", order.CustomerId);
+            ViewBag.EmployeeId = new SelectList(db.Employees, "Id", "FirstName", order.EmployeeId);
+            ViewBag.StatusId = new SelectList(db.OrderStatuses, "Id", "Name", order.StatusId);
+            ViewBag.ShipmentId = new SelectList(db.Shipments, "Id", "ShippingAddress", order.ShipmentId);
+            return View(order);
+        }
+
+        // GET: ADMIN/Orders/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: ADMIN/Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Order order = db.Orders.Find(id);
+            db.Orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
